2328a7f5632f538459f167339520a195ea022f61
HelplessSoldier <patrick1234143211@gmail.com>
2023-11-01 12:41:59 -0700

    added nav and got 404 working

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
+app.listen(3000);
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(morgan('dev'));
 
 app.use((req, res) => {
