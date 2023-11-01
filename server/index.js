46226f6e34274b44d468cb11f6ef8fac684f2e0f
HelplessSoldier <patrick1234143211@gmail.com>
2023-11-01 12:47:23 -0700

    added pages and routes

--- server/index.js
+++ server/index.js
@@ -9,6 +9,18 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(morgan('dev'));
 
+app.get('/', (req, res) => {
+  res.render('index');
+})
+
+app.get('/about', (req, res) => {
+  res.render('about');
+})
+
+app.get('/contact-me', (req, res) => {
+  res.render('contact');
+})
+
 app.use((req, res) => {
   res.status(400).render('404');
 })
