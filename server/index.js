231653fa9fe9ec237d857166d092f773a817d837
HelplessSoldier <patrick1234143211@gmail.com>
2023-11-01 12:57:01 -0700

    got stylesheet working

--- server/index.js
+++ server/index.js
@@ -8,7 +8,9 @@ app.listen(3000);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(morgan('dev'));
-app.use(express.static('public'));
+
+const publicDirPath = path.join(__dirname, 'public');
+app.use(express.static(publicDirPath));
 
 app.get('/', (req, res) => {
   res.render('index');
